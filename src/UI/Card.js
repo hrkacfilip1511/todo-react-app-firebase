83d57f847dad57981a739a74eef8439ccfbe7ec7
Filip Hrkac <filip1511@hotmail.com>
2022-04-12 14:34:44 +0200

    working with prop-types

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,7 +1,10 @@
 import classes from "./Card.module.css";
-
+import PropTypes from "prop-types";
 const Card = (props) => {
   return <div className={classes.card}>{props.children}</div>;
 };
 
+Card.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 export default Card;
